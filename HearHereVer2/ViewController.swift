07b61f2d5180d3336395c5ab79054241d3ebaac2
고영민 <Koh@goyeongmin-ui-macbook-pro.local>
2016-07-21 16:51:14 +0900

    Initial Commit

---
+++ HearHereVer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HearHereVer2
+//
+//  Created by 고영민 on 2016. 7. 21..
+//  Copyright © 2016년 고영민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HearHereVer2Tests/HearHereVer2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HearHereVer2Tests.swift
+//  HearHereVer2Tests
+//
+//  Created by 고영민 on 2016. 7. 21..
+//  Copyright © 2016년 고영민. All rights reserved.
+//
+
+import XCTest
+@testable import HearHereVer2
+
+class HearHereVer2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
